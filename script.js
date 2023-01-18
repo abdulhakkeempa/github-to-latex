c3ff5afd7374b80c06682de59a8b312c23924bd0
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-18 16:38:14 +0530

    js functionalities working

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 document.getElementById("record-form").onsubmit = (e) => {
   e.preventDefault();
   var form = document.getElementById("record-form")
-  fetch_url = `https://raw.githubusercontent.com/${form.github-username.value}/${form.github-repo.value}/main/${form.folder_structure.value}`
+  title = form.title.value
+  aim = form.aim.value
+  fetch_url = `https://raw.githubusercontent.com/${form.github_username.value}/${form.github_repo.value}/main/${form.folder_structure.value}`
+  fetch(fetch_url)
+  .then((response) => response.text())
+  .then((data) => getFormat(title,aim,data));
 }
 
+function getFormat(title,aim,data){
+  code = 
+  `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	\\begin{center}
+		%%Title of the Program
+		\\large\\textbf{${title}}
+	\\end{center}
+	
+	\\begin{flushleft}
+		\\textbf{AIM }
+	\\end{flushleft} 
+  %%Insert AIM of your program here
+        ${aim}    
+    \\begin{flushleft}
+      \\textbf{PROGRAM}
+    \\end{flushleft}
+  %%Insert the program code here
+    \\begin{verbatim}
+    ${data}
+    \\end{verbatim}
+    \\begin{flushleft}
+      \\textbf{SAMPLE INPUT-OUTPUT}
+    \\end{flushleft} 
+  %%Insert screen shot of sample output as image file as filename.png
+    \\includegraphics[scale=0.5]{}
+    \\includegraphics[scale=0.5]{}
+    \\newpage
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%`
+  document.getElementById("output").value += code
+}
+
+document.getElementById("copy-btn").onclick = () => {
+  navigator.clipboard.writeText(document.getElementById("output").value);
+}
