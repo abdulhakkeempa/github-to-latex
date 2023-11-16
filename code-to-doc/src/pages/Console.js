3053f87762dd29cfc666cf9022d3983a1552f1eb
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-11-16 10:51:37 +0530

    chore: added basic pages

---
+++ code-to-doc/src/pages/Console.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Console() {
+  return (
+    <div>
+      <h1>Console Page</h1>
+    </div>
+  );
+}
+
+export default Console;
---
+++ code-to-doc/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Auth() {
+  return (
+    <div>
+      <h1>Auth Page</h1>
+    </div>
+  );
+}
+
+export default Auth;
