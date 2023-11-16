fd1f9f6f160e0a5ee64c6335519860f899257700
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-11-16 11:48:59 +0530

    chore: integrated tailwind css and working on the login page

--- code-to-doc/src/App.js
+++ code-to-doc/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Console from './pages/Console';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/console" element={<Console />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
--- code-to-doc/src/pages/Login.js
+++ code-to-doc/src/pages/Login.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 function Auth() {
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className='text-5xl font-semibold text-white'>Code to Doc</h1>
+        {/* <div className='bg-black p-4 rounded'>
+          <div>
+            <h3 className='text-2xl text-white'>Sign Up / Sign In</h3>  
+          </div>
+          <div>
+            <button className='bg-white w-full py-2 rounded'>Continue with GitHub</button>
+          </div>
+        </div> */}
+        <div className='flex items-center justify-center'>
+          <div className='login p-4 w-96 h-56 flex flex-col items-center justify-center'>
+            <h3 className='text-2xl text-white mb-4'>Sign Up / Sign In</h3>
+            <button className='bg-white w-full py-2 rounded'>Continue with GitHub</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
+
 
 export default Auth;
